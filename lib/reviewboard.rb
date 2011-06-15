53f474f6341ccc13ed0dd709192c5ea23ac38d0d
Dale Fukami <dale.fukami@gmail.com>
2011-06-15 08:35:35 -0600

    Post a hard coded comment!

--- lib/reviewboard.rb
+++ lib/reviewboard.rb
@@ -1,3 +1,4 @@
+require 'typhoeus'
 require 'json'
 require 'pp'
 
@@ -8,8 +9,55 @@ class ReviewBoard
     @base_url = "http://*******/api"
   end
 
-  def draft(review_number)
-    response = make_request "review-requests/#{review_number}/"
+  def get_review_draft_id(review_number)
+    review_path = "review-requests/#{review_number}/reviews"
+    response = make_request "#{review_path}/draft/"
+    if response.code === 404
+      return nil
+    elsif response.code != 301
+      return nil
+    end
+
+    matches = response.headers_hash[:Location].match(".*#{review_path}/(\\d+)")
+    draft_id = matches[1]
+  end
+
+  def get_review_draft(review_number, review_id)
+    review_path = "review-requests/#{review_number}/reviews/#{review_id}"
+    response = make_request "#{review_path}/"
+
+    value = JSON.parse(response.body)
+    pp value
+  end
+
+  def get_review_draft_comments(review_number, review_id)
+    review_path = "review-requests/#{review_number}/reviews/#{review_id}/diff-comments"
+    response = make_request "#{review_path}/"
+
+    value = JSON.parse(response.body)
+    pp value
+  end
+
+  def get_review_draft_comment(review_number, review_id, comment_id)
+    review_path = "review-requests/#{review_number}/reviews/#{review_id}/diff-comments/#{comment_id}"
+    response = make_request "#{review_path}/"
+
+    value = JSON.parse(response.body)
+    pp value
+  end
+
+  def get_review_diffs(review_number)
+    review_path = "review-requests/#{review_number}/diffs/1/files"
+    response = make_request "#{review_path}/"
+
+    value = JSON.parse(response.body)
+    pp value
+  end
+
+  def post_review_draft_comment(review_number, review_id)
+    review_path = "review-requests/#{review_number}/reviews/#{review_id}/diff-comments"
+    response = post "#{review_path}/", {:first_line => 460, :text => 'my first comment', :num_lines => 3, :filediff_id => 25083}
+
     value = JSON.parse(response.body)
     pp value
   end
@@ -21,6 +69,15 @@ class ReviewBoard
       "#{@base_url}/#{resource}",
       :username => @user,
       :password => @pass
+    )
+  end
+
+  def post(resource, params)
+    Typhoeus::Request.post(
+      "#{@base_url}/#{resource}",
+      :username => @user,
+      :password => @pass,
+      :params => params
     )
   end
 end
--- review.rb
+++ review.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'typhoeus'
 
 require File.join( File.dirname(__FILE__), 'lib','reviewboard')
 
@@ -8,8 +7,12 @@ class Review
     @reviewboard = ReviewBoard.new
   end
 
-  def draft review_id
-    @reviewboard.draft(review_id)
+  def draft review_request_id
+    draft_id = @reviewboard.get_review_draft_id(review_request_id)
+#XXX     @reviewboard.get_review_draft_comments(review_request_id, draft_id)
+#XXX     @reviewboard.get_review_draft_comment(review_request_id, draft_id, 1992)
+#XXX     @reviewboard.get_review_diffs(review_request_id)
+    @reviewboard.post_review_draft_comment(review_request_id, draft_id)
   end
 end
 
